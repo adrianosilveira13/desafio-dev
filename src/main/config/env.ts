3b473ed5462e5f3306d9ddab3003f2aca867d058
Adriano <kdr@outlook.com.br>
2022-05-18 20:07:12 -0300

    feat: config express server

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   postgresUrl: process.env.POSTGRES_URL || 'http://localhost',
-  port: process.env.PORT,
+  port: process.env.PORT ?? 8080,
   jwtSecret: process.env.JWT_SECRET || 'asUFG978T(&*%'
 }
