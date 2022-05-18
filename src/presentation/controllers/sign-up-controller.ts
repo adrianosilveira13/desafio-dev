4e27e7c547211108d3cecbdba56d370b56c54f5e
Adriano <kdr@outlook.com.br>
2022-05-18 15:29:21 -0300

    feat: make SignUpController respect a default HttpRequest

--- src/presentation/controllers/sign-up-controller.ts
+++ src/presentation/controllers/sign-up-controller.ts
@@ -3,7 +3,16 @@ import { Validation } from '@/presentation/protocols'
 export class SignUpController {
   constructor (private readonly validation: Validation) {}
 
-  async handle (request: any): Promise<any> {
+  async handle (request: SignUpController.HttpRequest): Promise<any> {
     this.validation.validate(request)
+  }
+}
+
+export namespace SignUpController {
+  export type HttpRequest = {
+    name: string
+    email: string
+    password: string
+    passwordConfirmation: string
   }
 }
