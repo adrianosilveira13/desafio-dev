de1ea019029b256857d45e96b43ddd871eb41fe4
Adriano <kdr@outlook.com.br>
2022-05-18 16:07:49 -0300

    feat: add forbbiden http helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -4,3 +4,8 @@ export const badRequest = (error: Error): HttpResponse => ({
   statusCode: 400,
   body: error
 })
+
+export const forbbiden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error
+})
