1ceb5b42329ddf18b1d8555e908ab99aa4768569
Adriano <kdr@outlook.com.br>
2022-05-18 16:56:34 -0300

    feat: ensure AddAccountService returns false if CheckAccountByEmailRepository returns false

--- src/data/usecases/add-account-service.ts
+++ src/data/usecases/add-account-service.ts
@@ -5,7 +5,7 @@ export class AddAccountService implements AddAccount {
   constructor (private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository) {}
 
   async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
-    await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
-    return Promise.resolve(null)
+    const exists = await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
+    return !exists
   }
 }
