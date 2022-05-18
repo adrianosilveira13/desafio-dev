ab0b565b37099f1425fcc5ea4302146264db218d
Adriano <kdr@outlook.com.br>
2022-05-18 17:12:25 -0300

    feat: ensure AddAccountService calls AddAccountRepository with correct values

--- src/data/usecases/add-account-service.ts
+++ src/data/usecases/add-account-service.ts
@@ -1,17 +1,19 @@
 import { AddAccount } from '@/domain/usecases'
-import { CheckAccountByEmailRepository } from '@/data/protocols/db'
+import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols/db'
 import { Hasher } from '@/data/protocols/criptography'
 
 export class AddAccountService implements AddAccount {
   constructor (
     private readonly checkAccountByEmailRepository: CheckAccountByEmailRepository,
-    private readonly hasher: Hasher
+    private readonly hasher: Hasher,
+    private readonly addAccountRepository: AddAccountRepository
   ) {}
 
   async add (accountData: AddAccount.Params): Promise<AddAccount.Result> {
     const exists = await this.checkAccountByEmailRepository.checkByEmail(accountData.email)
     if (!exists) {
-      await this.hasher.hash(accountData.password)
+      const hashedPassword = await this.hasher.hash(accountData.password)
+      await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
     }
     return !exists
   }
