e0249453b71a35d18059a49f463203a55dc14df0
Adriano <kdr@outlook.com.br>
2022-05-18 21:19:13 -0300

    feat: ensure AuthenticationService calls HashComparer with correct values

--- src/data/usecases/authentication-service.ts
+++ src/data/usecases/authentication-service.ts
@@ -1,12 +1,17 @@
 import { Authentication } from '@/domain/usecases'
-import { LoadAccountByEmailRepository } from '../protocols/db'
+import { HashComparer } from '@/data/protocols/criptography'
+import { LoadAccountByEmailRepository } from '@/data/protocols/db'
 
 export class AuthenticationService implements Authentication {
-  constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+  constructor (
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
+  ) {}
 
   async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
     const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
     if (account) {
+      await this.hashComparer.compare(authenticationParams.password, account.password)
       return {
         accessToken: 'any_token',
         name: 'any_name'
