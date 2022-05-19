d58a900dd97d81f2ebcbe1827426e641a91d05f4
Adriano <kdr@outlook.com.br>
2022-05-19 11:12:53 -0300

    refactor: move type orm config to ormconfig file

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,9 +4,8 @@ import env from '@/main/config/env'
 
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
-import { config } from '@/infra/postgres/helpers'
 
-createConnection(config)
+createConnection()
   .then(() => {
     app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
   })
