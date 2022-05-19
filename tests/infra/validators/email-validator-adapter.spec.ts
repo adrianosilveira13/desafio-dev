9dd5cb9e9aac9d4e5b90cb4b2043e45cbad5a108
Adriano <kdr@outlook.com.br>
2022-05-19 20:19:57 -0300

    feat: ensure EmailValidatorAdapter returns false if Validator returns false

---
+++ tests/infra/validators/email-validator-adapter.spec.ts
@@ -0,0 +1,28 @@
+import { EmailValidatorAdapter } from '@/infra/validators'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (email: string): boolean {
+    return true
+  }
+}))
+
+type SutTypes = {
+  sut: EmailValidatorAdapter
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new EmailValidatorAdapter()
+  return {
+    sut
+  }
+}
+
+describe('EmailValidatorAdapter', () => {
+  it('should return false if validator returns false', () => {
+    const { sut } = makeSut()
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
+    const isValid = sut.isValid('invalid_email@mail.com')
+    expect(isValid).toBe(false)
+  })
+})
