4be95e61bb155f857542e540b47c71d5ae729897
Adriano <kdr@outlook.com.br>
2022-05-19 09:32:03 -0300

    feat: add PgUserAccountRepo factory

--- src/main/factories/services/authentication-factory.ts
+++ src/main/factories/services/authentication-factory.ts
@@ -1,12 +1,11 @@
 import env from '@/main/config/env'
 import { AuthenticationService } from '@/data/services'
 import { BcryptAdapter, JwtAdapter } from '@/infra/cryptography'
-import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { makePgUserAccountRepo } from '@/main/factories/repos'
 
 export const makeAuthenticationService = (): AuthenticationService => {
   const salt = 12
-  const pgUserAccountRepository = new PgUserAccountRepository()
   const bcryptAdapter = new BcryptAdapter(salt)
   const jwtAdatper = new JwtAdapter(env.jwtSecret)
-  return new AuthenticationService(pgUserAccountRepository, bcryptAdapter, jwtAdatper, pgUserAccountRepository)
+  return new AuthenticationService(makePgUserAccountRepo(), bcryptAdapter, jwtAdatper, makePgUserAccountRepo())
 }
