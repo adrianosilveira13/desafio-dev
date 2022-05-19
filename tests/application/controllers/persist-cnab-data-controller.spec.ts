69b5590e88ee7fb92f11e07d175c2c2417e6d7e1
Adriano <kdr@outlook.com.br>
2022-05-19 19:37:51 -0300

    test: ensure PersistCNABDataController returns 400 if Validation returns an error

--- tests/application/controllers/persist-cnab-data-controller.spec.ts
+++ tests/application/controllers/persist-cnab-data-controller.spec.ts
@@ -1,6 +1,7 @@
 import { ParseCNABSpy } from '@/tests/ultils/mocks'
 import { PersistCNABDataController } from '@/application/controllers'
 import { ValidationSpy } from '@/tests/application/mocks'
+import { badRequest } from '@/application/helpers'
 
 type SutTypes = {
   sut: PersistCNABDataController
@@ -29,6 +30,18 @@ describe('PersistCNABData Controller', () => {
     }
     await sut.handle(request)
     expect(validationSpy.input).toBe(request.file.buffer)
+  })
+
+  it('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+    const request = {
+      file: {
+        buffer: Buffer.from('any_buffer')
+      }
+    }
+    const response = await sut.handle(request)
+    expect(response).toEqual(badRequest(new Error()))
   })
 
   it('Should call ParseCNAB with correct value', async () => {
