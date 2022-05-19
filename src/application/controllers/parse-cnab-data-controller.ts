894fa9737328d1dcd2950baf077f8e15e21eb658
Adriano <kdr@outlook.com.br>
2022-05-19 19:36:54 -0300

    test: ensure PersistCNABDataController returns 400 if Validation returns an error

--- src/application/controllers/parse-cnab-data-controller.ts
+++ src/application/controllers/parse-cnab-data-controller.ts
@@ -1,4 +1,5 @@
 import { Controller, HttpResponse, ParseCNAB, Validation } from '@/application/protocols'
+import { badRequest } from '@/application/helpers'
 
 export class PersistCNABDataController implements Controller {
   constructor (
@@ -8,7 +9,8 @@ export class PersistCNABDataController implements Controller {
 
   async handle (request: PersistCNABDataController.HttpRequest): Promise<HttpResponse> {
     const buffer = request.file.buffer
-    await this.validation.validate(buffer)
+    const error = await this.validation.validate(buffer)
+    if (error) return badRequest(error)
     await this.parseCNAB.parse(buffer)
     return Promise.resolve(null)
   }
