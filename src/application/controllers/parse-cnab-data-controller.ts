8fdea0f89f320dcb882fa36b24e904b83e787402
Adriano <kdr@outlook.com.br>
2022-05-19 19:34:10 -0300

    feat: ensure PersistCNABDataController calls Validation with correct value

--- src/application/controllers/parse-cnab-data-controller.ts
+++ src/application/controllers/parse-cnab-data-controller.ts
@@ -1,10 +1,15 @@
-import { Controller, HttpResponse, ParseCNAB } from '@/application/protocols'
+import { Controller, HttpResponse, ParseCNAB, Validation } from '@/application/protocols'
 
 export class PersistCNABDataController implements Controller {
-  constructor (private readonly parseCNAB: ParseCNAB) {}
+  constructor (
+    private readonly validation: Validation,
+    private readonly parseCNAB: ParseCNAB
+  ) {}
 
   async handle (request: PersistCNABDataController.HttpRequest): Promise<HttpResponse> {
-    await this.parseCNAB.parse(request.file.buffer)
+    const buffer = request.file.buffer
+    await this.validation.validate(buffer)
+    await this.parseCNAB.parse(buffer)
     return Promise.resolve(null)
   }
 }
