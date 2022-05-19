146f42fdc63a33385bfd57840a7754cd714f5664
Adriano <kdr@outlook.com.br>
2022-05-19 09:35:13 -0300

    refactor: change add-account factory to use other factories

--- src/main/factories/services/add-account-factory.ts
+++ src/main/factories/services/add-account-factory.ts
@@ -1,11 +1,8 @@
 import { AddAccountService } from '@/data/services'
 import { AddAccount } from '@/domain/usecases'
-import { BcryptAdapter } from '@/infra/cryptography'
-import { PgUserAccountRepository } from '@/infra/postgres/repos'
+import { makeBcryptAdapter } from '../crypto'
+import { makePgUserAccountRepo } from '../repos'
 
 export const makeAddAccountService = (): AddAccount => {
-  const salt = 12
-  const bcryptAdapter = new BcryptAdapter(salt)
-  const userAccountRepository = new PgUserAccountRepository()
-  return new AddAccountService(userAccountRepository, bcryptAdapter, userAccountRepository)
+  return new AddAccountService(makePgUserAccountRepo(), makeBcryptAdapter(), makePgUserAccountRepo())
 }
