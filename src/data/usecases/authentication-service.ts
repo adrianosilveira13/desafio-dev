a48fd9acc9505c035d858f456a6e859349d16a5d
Adriano <kdr@outlook.com.br>
2022-05-18 21:12:26 -0300

    feat: ensure AuthenticationService returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication-service.ts
+++ src/data/usecases/authentication-service.ts
@@ -5,7 +5,13 @@ export class AuthenticationService implements Authentication {
   constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
 
   async auth (authenticationParams: Authentication.Params): Promise<Authentication.Result> {
-    await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
-    return Promise.resolve(null)
+    const account = await this.loadAccountByEmailRepository.loadByEmail(authenticationParams.email)
+    if (account) {
+      return {
+        accessToken: 'any_token',
+        name: 'any_name'
+      }
+    }
+    return null
   }
 }
