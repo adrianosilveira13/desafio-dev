0b362e5cbf4615ddc84bb24d71b8d41e5c74a759
Adriano <kdr@outlook.com.br>
2022-05-18 21:56:37 -0300

    test: add jwt adapter

---
+++ tests/infra/cryptography/jwt-adapter.spec.ts
@@ -0,0 +1,42 @@
+import { JwtAdapter } from '@/infra/cryptography'
+import jwt from 'jsonwebtoken'
+
+jest.mock('jsonwebtoken', () => ({
+  sign: async (): Promise<string> => {
+    return Promise.resolve('any_token')
+  },
+
+  verify: async (): Promise<string> => {
+    return Promise.resolve('any_value')
+  }
+}))
+
+const makeSut = (): JwtAdapter => {
+  return new JwtAdapter('secret')
+}
+
+describe('Jwt Adapter', () => {
+  describe('sign()', () => {
+    it('Should call sign with correct values', async () => {
+      const sut = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
+      await sut.encrypt('any_id')
+      expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+    })
+
+    it('Should return a token on sign success', async () => {
+      const sut = makeSut()
+      const accessToken = await sut.encrypt('any_id')
+      expect(accessToken).toBe('any_token')
+    })
+
+    it('Should throw if sign throws', async () => {
+      const sut = makeSut()
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(async () => {
+        return Promise.reject(new Error())
+      })
+      const promise = sut.encrypt('any_id')
+      await expect(promise).rejects.toThrow()
+    })
+  })
+})
