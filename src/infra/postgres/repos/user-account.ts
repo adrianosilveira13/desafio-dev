d289790990c4058f5794d198e5af50962eab0328
Adriano <kdr@outlook.com.br>
2022-05-18 21:44:22 -0300

    feat: ensure PgUserAccountRepository returns null if loadByEmail fails

--- src/infra/postgres/repos/user-account.ts
+++ src/infra/postgres/repos/user-account.ts
@@ -1,8 +1,8 @@
-import { AddAccountRepository, CheckAccountByEmailRepository } from '@/data/protocols/db'
+import { AddAccountRepository, CheckAccountByEmailRepository, LoadAccountByEmailRepository } from '@/data/protocols/db'
 import { getRepository } from 'typeorm'
 import { PgUser } from '@/infra/postgres/entities'
 
-export class PgUserAccountRepository implements AddAccountRepository, CheckAccountByEmailRepository {
+export class PgUserAccountRepository implements AddAccountRepository, CheckAccountByEmailRepository, LoadAccountByEmailRepository {
   async add (data: AddAccountRepository.Params): Promise<AddAccountRepository.Result> {
     const pgUserRepo = getRepository(PgUser)
     const account = await pgUserRepo.save({
@@ -17,5 +17,16 @@ export class PgUserAccountRepository implements AddAccountRepository, CheckAccou
     const pgUserRepo = getRepository(PgUser)
     const account = await pgUserRepo.findOne({ email })
     return account !== undefined
+  }
+
+  async loadByEmail (email: string): Promise<LoadAccountByEmailRepository.Result> {
+    const pgUserRepo = getRepository(PgUser)
+    const account = await pgUserRepo.findOne({ email })
+    if (!account) return null
+    return {
+      id: (account.id).toString(),
+      name: account.name,
+      password: account.password
+    }
   }
 }
