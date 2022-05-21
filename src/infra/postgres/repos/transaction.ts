b6046f303338977b890bdc1ce1412cb19c40da6f
Adriano <kdr@outlook.com.br>
2022-05-21 13:23:37 -0300

    feat: ensure PersistCNABService createStore method returns an id on success

--- src/infra/postgres/repos/transaction.ts
+++ src/infra/postgres/repos/transaction.ts
@@ -1,8 +1,8 @@
-import { CheckStoreByNameAndOwnerRepository, CheckTransactionTypeRepository } from '@/data/protocols/db'
+import { CheckStoreByNameAndOwnerRepository, CheckTransactionTypeRepository, CreateStoreRepository } from '@/data/protocols/db'
 import { getRepository } from 'typeorm'
 import { PgStore, PgTransactionType } from '../entities'
 
-export class PgTransactionRepository implements CheckTransactionTypeRepository, CheckStoreByNameAndOwnerRepository {
+export class PgTransactionRepository implements CheckTransactionTypeRepository, CheckStoreByNameAndOwnerRepository, CreateStoreRepository {
   async checkByType (type: number): Promise<boolean> {
     const transactionTypeRepo = getRepository(PgTransactionType)
     const transactionType = await transactionTypeRepo.findOne({ id: type })
@@ -13,6 +13,13 @@ export class PgTransactionRepository implements CheckTransactionTypeRepository, 
   async checkStore (params: CheckStoreByNameAndOwnerRepository.Params): Promise<CheckStoreByNameAndOwnerRepository.Result> {
     const storeRepo = getRepository(PgStore)
     const store = await storeRepo.findOne({ owner: params.owner, name: params.storeName })
+    if (!store) return null
+    return { id: store.id }
+  }
+
+  async createStore (params: CreateStoreRepository.Params): Promise<CreateStoreRepository.Result> {
+    const storeRepo = getRepository(PgStore)
+    const store = await storeRepo.save({ owner: params.owner, name: params.storeName })
     if (!store) return null
     return { id: store.id }
   }
