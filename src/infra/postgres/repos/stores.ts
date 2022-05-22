54d859ecc7d4861f14318276a150b63095d0c004
Adriano <kdr@outlook.com.br>
2022-05-22 16:22:57 -0300

    feat: ensure LoadStoresRepository returns null if no store were found

--- src/infra/postgres/repos/stores.ts
+++ src/infra/postgres/repos/stores.ts
@@ -5,10 +5,11 @@ import { PgStore } from '../entities'
 export class PgStoresRepository implements LoadStoresRepository {
   async load (): Promise<LoadStoresRepository.Result> {
     const storesRepo = getRepository(PgStore)
-    const stores = storesRepo
+    const stores = await storesRepo
       .createQueryBuilder()
       .select(['id, name AS "storeName", owner'])
       .getRawMany()
+    if (stores.length === 0) return null
     return stores
   }
 }
