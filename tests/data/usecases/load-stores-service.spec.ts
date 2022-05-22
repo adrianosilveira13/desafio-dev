10a5dcdbc5c7d47a2b7031f18f2c77485eeef445
Adriano <kdr@outlook.com.br>
2022-05-22 15:40:56 -0300

    test: ensure LoadStoresService calls LoadStoresRepository with correct values

--- tests/data/usecases/load-stores-service.spec.ts
+++ tests/data/usecases/load-stores-service.spec.ts
@@ -1,5 +1,19 @@
-describe('LoadStoresService Usecase', () => {
-  it('', () => {
+import { LoadStoresService } from '@/data/services'
+import { LoadStoresRepository } from '@/data/protocols/db/stores'
 
+describe('LoadStoresService Usecase', () => {
+  it('Should call LoadStoresRepository', async () => {
+    class LoadStoresRepositorySpy implements LoadStoresRepository {
+      callsCount = 0
+
+      async load (): Promise<LoadStoresRepository.Result> {
+        this.callsCount++
+        return Promise.resolve(null)
+      }
+    }
+    const loadStoresRepositorySpy = new LoadStoresRepositorySpy()
+    const sut = new LoadStoresService(loadStoresRepositorySpy)
+    await sut.load()
+    expect(loadStoresRepositorySpy.callsCount).toBe(1)
   })
 })
