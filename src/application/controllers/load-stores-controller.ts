2c212929283b093e2e99a8feb5e28de78f83b5d4
Adriano <kdr@outlook.com.br>
2022-05-22 12:49:12 -0300

    feat: ensure LoadStoresController returns 500 if LoadStores throws

--- src/application/controllers/load-stores-controller.ts
+++ src/application/controllers/load-stores-controller.ts
@@ -1,13 +1,17 @@
 import { Controller, HttpResponse } from '@/application/protocols'
 import { LoadStores } from '@/domain/usecases'
-import { notFound, ok } from '@/application/helpers'
+import { notFound, ok, serverError } from '@/application/helpers'
 
 export class LoadStoresController implements Controller {
   constructor (private readonly loadStores: LoadStores) {}
 
   async handle (): Promise<HttpResponse> {
-    const stores = await this.loadStores.load()
-    if (stores === null) return notFound()
-    return ok(stores)
+    try {
+      const stores = await this.loadStores.load()
+      if (stores === null) return notFound()
+      return ok(stores)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
